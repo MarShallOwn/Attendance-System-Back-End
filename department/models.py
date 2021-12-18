f5482a7a9f9f0ce6a84399b78ae1690d8ed02319
mustafasaif29 <61163729+mustafasaif29@users.noreply.github.com>
2021-12-18 17:10:29 +0200

    finish department endpoint

---
+++ department/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+from authentication.models import User
+import uuid
+from helpers.models import TrackingModel
+
+# Create your models here.
+
+class department(TrackingModel,models.Model):
+    id = models.UUIDField(
+    primary_key=True,
+    unique=True,
+    auto_created=True,
+    default=uuid.uuid4,
+    editable=False,
+    null=False,
+    blank=False,
+    ) 
+    departmentName = models.CharField(max_length=20,blank=False,null=False)
+    departmentDesc = models.TextField(blank=False,null=False)
+    departmentHeadID = models.OneToOneField(User,related_name='department',on_delete=models.SET_NULL,null=True,blank=False)
+    def __str__(self):
+        return self.departmentName
