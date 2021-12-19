91e777a1830f94f2207385f5eeff2bc43ee1e3ef
mustafasaif29 <61163729+mustafasaif29@users.noreply.github.com>
2021-12-19 18:28:12 +0200

    finish role crud

---
+++ role/views.py
@@ -0,0 +1,50 @@
+from Common_Responses import Bad_Response, Created_Response, No_Content_Response, Ok_Response
+from role.serializers import roleserializer
+from .models import role
+from rest_framework.decorators import api_view
+
+# Create your views here.
+#GET-Post
+@api_view(['Get','Post'])
+def role_List(request):
+    if request.method == 'GET':
+        try:
+            roles = role.objects.all()
+        except:
+            return Bad_Response(data=None,From='Get role')
+        serializer = roleserializer(roles, many=True)
+        return Ok_Response(serializer.data)
+
+    elif request.method == 'POST':
+        serializer = roleserializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Created_Response(serializer.data)
+        else:
+            return Bad_Response(data=serializer.errors,From=None)
+    else:
+            return Bad_Response('All roles')
+
+@api_view(['GET','PUT','DELETE'])
+def role_pk(request, pk):
+    try:
+         roles = role.objects.get(pk=pk)
+    except role.DoesNotExist:
+        return Bad_Response(data= None,From='GET role pk')
+         
+    if request.method == 'GET':
+        serializer = roleserializer(roles)
+        return Ok_Response(serializer.data)
+
+    elif request.method == 'PUT':
+        serializer = roleserializer(role, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return No_Content_Response()
+        else:
+            return Bad_Response(data=serializer.errors,From=None)
+    elif request.method == 'DELETE':
+            role.delete()
+            return No_Content_Response()
+    else:
+        return Bad_Response('All role pk')
