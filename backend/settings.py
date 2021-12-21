ece2c1fb55a6d61cc7fd4a7e501f020c6aff8a39
Marwan Samih <53130198+MarShallOwn@users.noreply.github.com>
2021-12-21 05:07:48 +0200

    Fix settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -97,25 +97,26 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+"""
 
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.mysql', 
-#         'NAME': os.environ.get("DB_NAME"),
-#         'USER': os.environ.get("DB_USER"),
-#         'PASSWORD': os.environ.get("DB_PASSWORD"),
-#         'HOST': os.environ.get("DB_HOST"),   # Or an IP Address that your DB is hosted on
-#         'PORT': os.environ.get("DB_PORT"),
-#     }
-# }
+DATABASES = {
+     'default': {
+         'ENGINE': 'django.db.backends.mysql', 
+         'NAME': os.environ.get("DB_NAME"),
+         'USER': os.environ.get("DB_USER"),
+         'PASSWORD': os.environ.get("DB_PASSWORD"),
+         'HOST': os.environ.get("DB_HOST"),   # Or an IP Address that your DB is hosted on
+         'PORT': os.environ.get("DB_PORT"),
+     }
+}
 
 
 # Password validation
