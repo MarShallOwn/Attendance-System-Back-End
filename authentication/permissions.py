68d5036cdf3398a9870f39fe00884cca3734720f
abdo-giprel <abdo.gebril2000@example.com>
2021-12-22 02:00:18 +0200

    defuat value to user,control.in empty migration

--- authentication/permissions.py
+++ authentication/permissions.py
@@ -1,10 +1,10 @@
 def IsEmployer(request):
-    if str(request.user.role)=="f2438952-9698-45b5-8c19-47bc5a1cd1ea":
+    if str(request.user.role.roleName)=="manager":
         return True
     return False
           
 
 def IsManager(request):
-    if str(request.user.role)=="1c1a9a99-1498-41cd-a8d6-6292b58867b0":
+    if str(request.user.role.roleName)=="employer":
         return True
     return False
--- systemControl/urls.py
+++ systemControl/urls.py
@@ -1,9 +1,6 @@
 from django.urls import path
 from . import views
 urlpatterns =[
-    path('create',views.SystemControl_List,name='systemcontrolcreate'),
-    path('list',views.SystemControl_List,name='systemcontrollist'),
-    path('get/<uuid:pk>',views.SystemControl_pk,name='systemcontrolget'),
-    path('update/<uuid:pk>',views.SystemControl_pk,name='systemcontrolupdate'),
-    path('delete/<uuid:pk>',views.SystemControl_pk,name='systemcontroldelete'), 
+    path('get',views.SystemControl_List,name='systemcontrolget'),
+    path('update',views.SystemControl_pk,name='systemcontrolupdate'),
 ]
