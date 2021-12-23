1905f3f0ad777709f2af7cea8721284170f7c56c
abdo-giprel <abdo.gebril2000@example.com>
2021-12-24 00:02:50 +0200

    edit the update url of department

--- department/urls.py
+++ department/urls.py
@@ -4,6 +4,6 @@ urlpatterns =[
     path('create',views.Department_List,name='departmentcreate'),
     path('list',views.Department_List,name='departmentlist'),
     path('get/<uuid:pk>',views.Department_pk,name='departmentget'),
-    path('edit/<uuid:pk>',views.Department_pk,name='departmentupdate'),
+    path('edit/<uuid:pk>',views.Department_pk,name='departmentedit'),
     path('delete/<uuid:pk>',views.Department_pk,name='departmentdelete'), 
 ]
